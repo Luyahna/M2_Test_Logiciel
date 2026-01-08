7a1462bf369efc2b9b2da372e59a1d979b1e9ec3
Lucie Bories <lucie.bories01@gmail.com>
2026-01-08 11:55:47 +0100

    Add files via upload
    Ensemble des fichiers pour les fonctions de recherche dans un tableau et de racine carré entière avec l'oracle de chacune, développé durant les séances de TP

---
+++ search/oracle_search.c
@@ -0,0 +1,43 @@
+#include <stdbool.h>
+void oracle_search(
+  int *Pre_A, int *A,
+  int Pre_elem, int elem,
+  int pathcrawler__retres__search)
+{
+    int i = 0;
+    int res =  pathcrawler__retres__search ;
+    bool elem_found = false ;
+
+    for (i = 0 ; i < 8 ; i++)
+    {
+        if (elem == A[i])
+        {
+            elem_found = true;
+            break;
+
+        } else if (elem != A[i])
+        {
+            elem_found = false ;
+        }
+    }
+
+    if (elem_found == false && res == -1)
+    {
+        pathcrawler_verdict_success();
+
+    }else if (elem_found == false && res != -1)
+    {
+        pathcrawler_verdict_failure();
+    }
+
+    if (elem_found == true && res == i)
+    {
+        pathcrawler_verdict_success();
+
+    } else if (elem_found == true && res != i)
+    {
+        pathcrawler_verdict_failure();
+    }
+
+    return;
+}
---
+++ sqrt/oracle_sqrt.c
@@ -0,0 +1,29 @@
+void oracle_sqrt(
+  int Pre_a, int a,
+  int pathcrawler__retres__sqrt)
+{
+  int i = pathcrawler__retres__sqrt;
+
+  if (a>0)
+  {
+    if (i^2<=a && a<(i+1)^2)
+    {
+        pathcrawler_verdict_success();
+
+    }else
+    {
+        pathcrawler_verdict_failure();
+    }
+  }else{
+    if(i==0)
+    {
+        pathcrawler_verdict_success();
+    }else
+    {
+        pathcrawler_verdict_failure();
+    }
+  }
+
+  return;
+
+}
